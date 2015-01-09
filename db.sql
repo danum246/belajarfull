4384ce8ea1fbad4d2c8ac74dea1a75c335afc23e
danum246 <danum246@gmail.com>
2015-01-10 02:16:44 +0700

    1

---
+++ db.sql
@@ -0,0 +1,37 @@
+/*
+SQLyog Ultimate v11.11 (32 bit)
+MySQL - 5.1.33-community : Database - db_belajar
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`db_belajar` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `db_belajar`;
+
+/*Table structure for table `tbl_user_login` */
+
+DROP TABLE IF EXISTS `tbl_user_login`;
+
+CREATE TABLE `tbl_user_login` (
+  `id_user` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `password_plain` varchar(50) DEFAULT NULL,
+  `status` int(11) DEFAULT NULL COMMENT '1 = aktif , 0 = gak',
+  PRIMARY KEY (`id_user`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `tbl_user_login` */
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
